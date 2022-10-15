cc2d6770ebd104c274cf293f72b8c211d8baf2b5
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-15 11:44:34 -0500

    init 4

--- lib/pages/login1_page.dart
+++ lib/pages/login1_page.dart
@@ -35,171 +35,193 @@ class Login1Page extends StatelessWidget {
           SafeArea(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 40.0,
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(16.0),
-                    decoration: BoxDecoration(
-                        color: kSecondaryColorLogin1.withOpacity(0.16),
-                        shape: BoxShape.circle),
-                    child: Icon(
-                      Icons.home_filled,
-                      color: kSecondaryColorLogin1,
-                      size: 38,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 40.0,
                     ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Text(
-                    "Let's log you in",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.bold,
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                          color: kSecondaryColorLogin1.withOpacity(0.16),
+                          shape: BoxShape.circle),
+                      child: Icon(
+                        Icons.home_filled,
+                        color: kSecondaryColorLogin1,
+                        size: 38,
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Text(
-                    "Welcome back You're been missed!",
-                    style: TextStyle(
-                      color: Colors.white54,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.bold,
+                    SizedBox(
+                      height: 30.0,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 36.0,
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: SizedBox(
-                          height: 50.0,
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xffEA4335),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
+                    Text(
+                      "Let's log you in",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      "Welcome back You're been missed!",
+                      style: TextStyle(
+                        color: Colors.white54,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 36.0,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: SizedBox(
+                            height: 50.0,
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xffEA4335),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
                               ),
-                            ),
-                            icon: SvgPicture.asset(
-                              'assets/images/google.svg',
-                              color: Colors.white,
-                            ),
-                            label: Text(
-                              "Google",
+                              icon: SvgPicture.asset(
+                                'assets/images/google.svg',
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                "Google",
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        width: 16.0,
-                      ),
-                      Expanded(
-                        child: SizedBox(
-                          height: 50.0,
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.blue,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
+                        const SizedBox(
+                          width: 16.0,
+                        ),
+                        Expanded(
+                          child: SizedBox(
+                            height: 50.0,
+                            child: ElevatedButton.icon(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blue,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
                               ),
-                            ),
-                            icon: SvgPicture.asset(
-                              'assets/images/facebook.svg',
-                              color: Colors.white,
-                            ),
-                            label: Text(
-                              "Facebook",
+                              icon: SvgPicture.asset(
+                                'assets/images/facebook.svg',
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                "Facebook",
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-
-                  const SizedBox(
-                    height: 36.0,
-                  ),
-
-                  TextField(
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-                      hintText: "Username",
-                      hintStyle: const TextStyle(
-                        color: Colors.white70,
-                        fontSize: 14.0
-                      ),
-                      filled: true,
-                      fillColor: Colors.white.withOpacity(0.06),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
-                      ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(
-                    height: 18.0,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-                      hintText: "Email Address",
-                      hintStyle: const TextStyle(
+
+                    const SizedBox(
+                      height: 36.0,
+                    ),
+
+                    TextField(
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
+                        hintText: "Username",
+                        hintStyle: const TextStyle(
                           color: Colors.white70,
                           fontSize: 14.0
-                      ),
-                      filled: true,
-                      fillColor: Colors.white.withOpacity(0.06),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 18.0,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
-                      hintText: "Password",
-                      hintStyle: const TextStyle(
-                          color: Colors.white70,
-                          fontSize: 14.0
-                      ),
-                      filled: true,
-                      fillColor: Colors.white.withOpacity(0.06),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12.0),
-                        borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.06),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
                       ),
                     ),
-                  ),
+                    const SizedBox(
+                      height: 18.0,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
+                        hintText: "Email Address",
+                        hintStyle: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14.0
+                        ),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.06),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 18.0,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 16.0),
+                        hintText: "Password",
+                        hintStyle: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 14.0
+                        ),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.06),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
 
 
-                ],
+                    const  SizedBox(
+                      height: 30.0,
+                    ),
+
+                    SizedBox(
+                      height: 52,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: kSecondaryColorLogin1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0)
+                          )
+                        ),
+                        child: Text("Sign Up"),
+                        onPressed: (){},
+                      ),
+                    ),
+
+
+                  ],
+                ),
               ),
             ),
           ),
