fb28738a91d6128075c30ff398e0f03d106f8aca
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-16 11:55:21 -0500

    Init 2 1.1

--- lib/pages/init2_page.dart
+++ lib/pages/init2_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 class Init2Page extends StatelessWidget {
@@ -8,7 +6,25 @@ class Init2Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Image.network(
+            "https://images.pexels.com/photos/2474689/pexels-photo-2474689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
