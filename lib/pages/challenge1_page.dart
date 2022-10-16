9153c1202c223387d702101ef4a48c332dd1ccc6
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-16 12:55:17 -0500

    Challenge 4

--- lib/pages/challenge1_page.dart
+++ lib/pages/challenge1_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class Challenge1Page extends StatelessWidget {
@@ -6,8 +5,65 @@ class Challenge1Page extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    double height = MediaQuery.of(context).size.height;
 
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Image.network(
+            "https://images.pexels.com/photos/4545333/pexels-photo-4545333.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            height: height * 0.5,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              color: Colors.blue,
+              width: double.infinity,
+              padding: EdgeInsets.all(22.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Sacramento",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0
+                    ),
+                  ),
+                  Text(
+                    "Adam Gontier",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "About",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteu",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
