708deb197d38d5a6bb510b533fc8ac8a64b371ec
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-16 12:07:43 -0500

    Init 2 1.2

--- lib/pages/init2_page.dart
+++ lib/pages/init2_page.dart
@@ -9,19 +9,40 @@ class Init2Page extends StatelessWidget {
       body: Stack(
         children: [
           Image.network(
-            "https://images.pexels.com/photos/2474689/pexels-photo-2474689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "https://images.pexels.com/photos/2499802/pexels-photo-2499802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
             fit: BoxFit.cover,
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              height: 300,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.amber,
-                borderRadius: BorderRadius.circular(20.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(40.0),
+                ),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Username",
+                        hintStyle: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black45,
+                        ),
+                        filled: true,
+                        fillColor: Color(0xffF5F5F5),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
