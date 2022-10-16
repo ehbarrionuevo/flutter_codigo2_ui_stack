064ba4817297471537aaca6766b29295d7917623
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-10-16 12:19:06 -0500

    Init 2 1.3

--- lib/pages/init2_page.dart
+++ lib/pages/init2_page.dart
@@ -7,6 +7,7 @@ class Init2Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
+        fit: StackFit.expand,
         children: [
           Image.network(
             "https://images.pexels.com/photos/2499802/pexels-photo-2499802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
@@ -16,29 +17,94 @@ class Init2Page extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
-                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0),
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 36.0),
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  color: Colors.amber,
-                  borderRadius: BorderRadius.circular(40.0),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    topLeft: Radius.circular(30.0),
+                  ),
                 ),
                 child: Column(
                   children: [
+                    const SizedBox(
+                      height: 14.0,
+                    ),
                     TextField(
                       decoration: InputDecoration(
                         hintText: "Username",
-                        hintStyle: TextStyle(
+                        hintStyle: const TextStyle(
                           fontSize: 14.0,
                           color: Colors.black45,
                         ),
                         filled: true,
-                        fillColor: Color(0xffF5F5F5),
+                        fillColor: const Color(0xffF5F5F5),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
                         enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(14),
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
                           borderSide: BorderSide.none,
                         ),
                       ),
                     ),
+                    const SizedBox(
+                      height: 14.0,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Password",
+                        hintStyle: const TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black45,
+                        ),
+                        filled: true,
+                        fillColor: const Color(0xffF5F5F5),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 14.0),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12.0,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("Forgot password?",),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 14.0,
+                    ),
+
+                    SizedBox(
+                      height: 54.0,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40.0),
+                          ),
+                        ),
+                        child: Text("Sign in",),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 14.0,
+                    ),
+
                   ],
                 ),
               ),
